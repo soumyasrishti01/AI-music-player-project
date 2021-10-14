564f12178c07100a4560d49a4854aa3c6f250b82
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-10-14 11:34:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 var BTS_BUTTER = "";
 var BTS_PTD = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
-function preload(){
+function preload() {
     BTS_BUTTER = "BTS_BUTTER.MP3";
     BTS_PTD = "BTS_PTD.mp3";
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(500, 400);
     canvas.position(385, 200);
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 
-function draw(){
+function modelLoaded() {
+    console.log("PoseNet Is Initialized")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+
+function draw() {
     image(video, 0, 0, 500, 400);
 }
